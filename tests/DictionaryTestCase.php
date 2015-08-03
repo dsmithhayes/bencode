28a795be824596c9144deb66a218db85539f8dc8
Dave Smith-Hayes <dsmithhayes@propellerideas.com>
2015-08-03 16:56:03 -0400

    Dictionary tests.

---
+++ tests/DictionaryTestCase.php
@@ -0,0 +1,26 @@
+<?php
+
+use DSH\Bencode\Dictionary;
+use DSH\Bencode\ElementList;
+use DSH\Bencode\Integer;
+use DSH\Bencode\Byte;
+
+class DictionaryTestCase extends PHPUnit_Framework_TestCase
+{
+	private $_dictionary = array(5 => 'test', 'key' => 2);
+	private $_encoded = 'di5e4:test3:keyi2ee';
+	
+	public function testDictionaryCreation()
+	{
+		$dictionary = new Dictionary();
+		$array_dictionary = new Dictionary($this->_dictionary);
+		$stream_dictionary = new Dictionary($this->_encoded);
+	}
+	
+	public function testDictionaryEncoding()
+	{
+		$dictionary = new Dictionary($this->_dictionary);
+		
+		// $this->assertEquals($this->_encoded, $dictionary->encode());
+	}
+}

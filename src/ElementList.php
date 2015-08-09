9add749acb370d4fac263621fd7f9f849417b302
Dave Smith-Hayes <dsmithhayes@propellerideas.com>
2015-08-09 10:57:48 -0400

    Rid myself of exceptions that aren't important.

--- src/ElementList.php
+++ src/ElementList.php
@@ -5,6 +5,7 @@ namespace DSH\Bencode;
 use DSH\Bencode\Core\Element;
 use DSH\Bencode\Core\Buffer;
 use DSH\Bencode\Exceptions\ElementListException;
+use DSH\Stack\Stack;
 
 class ElementList implements Element, Buffer
 {
@@ -12,14 +13,17 @@ class ElementList implements Element, Buffer
 	
 	protected $_buffer;
 	
-	public function __construction($buffer = array())
+	public function __construct($buffer = array())
 	{
-		
+		$this->read($buffer);
 	}
 	
 	public function encode()
 	{
+		foreach($this->_buffer as $b)
+			$buffer .= $b;
 		
+		return 'l' . $buffer . 'e';
 	}
 	
 	public function decode($stream)
@@ -29,11 +33,14 @@ class ElementList implements Element, Buffer
 	
 	public function write()
 	{
-		
+		return $this->_buffer;
 	}
 	
 	public function read($value)
 	{
-		
+		if(is_array($value))
+			$this->_buffer = $value;
+		else
+			$this->_buffer[] = $value;
 	}
 }

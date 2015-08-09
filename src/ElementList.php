3c9569e20bc79eea1e8ae595527af4b793ddb123
Dave Smith-Hayes <dsmithhayes@propellerideas.com>
2015-08-09 10:31:42 -0400

    Progress.

---
+++ src/ElementList.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace DSH\Bencode;
+
+use DSH\Bencode\Core\Element;
+use DSH\Bencode\Core\Buffer;
+use DSH\Bencode\Exceptions\ElementListException;
+
+class ElementList implements Element, Buffer
+{
+	
+}

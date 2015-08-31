98b2fecbb8e4d82bf5e73751af2372ada33c7c79
Dave Smith-Hayes <dsmithhayes@gmail.com>
2015-08-30 22:47:23 -0400

    Wrote an encoder, documented the crap out of the Dictionary.

--- src/Collection/Dictionary.php
+++ src/Collection/Dictionary.php
@@ -17,18 +17,45 @@ use DSH\Bencode\Exception\Dictionary;
 
 class Dictionary implements Element, Buffer
 {
+    /**
+     * @const The regex pattern that matches an encoded Dictionary
+     */
     const PATTERN = '/^d.*e$/';
-    
+
+    /**
+     * @var array $_buffer the internal buffer array
+     */
     protected $_buffer;
-    
+
+    /**
+     * Initialize with an array or empty. What ever.
+     *
+     * @param array $buffer the array to become the buffer.
+     */
     public function __construct($buffer = [])
     {
         $this->read($buffer);
     }
 
+    /**
+     * Returns an encoded stream of a Dictionary element.
+     *
+     * @return string Encoded Dictionary
+     */
     public function encode()
     {
+        $buffer = 'd';
+        
+        foreach ($this->_buffer as $key => $val) {
+            $key = new Byte($key);
+            $val = new Byte($val);
 
+            $buffer .= $key->encode() . $val->encode();
+        }
+
+        $buffer .= 'e';
+
+        return $buffer;
     }
 
     /**
@@ -36,13 +63,18 @@ class Dictionary implements Element, Buffer
      * we are looking for the firs two Elements and setting them as
      * the key and value of the array.
      *
-     * @param string $stream an encoded Dictionary
+     * @param string $stream Encoded Dictionary
      */
     public function decode($stream)
     {
 
     }
 
+    /**
+     * Gets the internal buffer of the Dictionary object.
+     *
+     * @return array Internal buffer
+     */
     public function write()
     {
         return $this->_buffer;
@@ -53,13 +85,18 @@ class Dictionary implements Element, Buffer
      * either be a key-value array, or an associative array with the
      * first value being the key. All keys must be Bytes.
      * 
-     * @param array $value Must be a key-value array.
+     * @param array $value Key-value array
      */
     public function read($value)
     {
         
     }
 
+    /**
+     * Encodes the internal buffer as JSON
+     *
+     * @return string JSON encoded internal buffer
+     */
     public function json()
     {
         return json_encode($this->_buffer);

9a04de39bf2c7915bceac8fbf405abf8e9c15737
Dave Smith-Hayes <dsmithhayes@propellerideas.com>
2015-09-05 14:42:37 -0400

    Touched some files. Building the Bencode object (just a giant factory)

--- src/Bencode.php
+++ src/Bencode.php
@@ -2,7 +2,62 @@
 
 namespace DSH\Bencode;
 
+use DSH\Bencode\Integer;
+use DSH\Bencode\Byte;
+use DSH\Bencode\Collection\BList;
+use DSH\Bencode\Collection\Dictionary;
+
 class Bencode
 {
-
+    /**
+     * Returns an instance of an Integer.
+     *
+     * @param integer  $int The value to store in the Integer buffer
+     * @param callable $cb  A callback function on the instantiated Integer.
+     *
+     * @return \DSH\Bencode\Integer An Integer object
+     */
+    public static function integerFactory($int = null, callable $cb = null)
+    {
+        $integer = new Integer($value);
+        
+        if ($cb) {
+            $integer = $cb($integer);
+        }
+        
+        return $intger;
+    }
+    
+    public static function byteFactory($byte = null, callable $cb = null)
+    {
+        $byte = new Byte($value);
+        
+        if ($cb) {
+            $byte = $cb($byte);
+        }
+        
+        return $byte;
+    }
+    
+    public static function blistFactory($blist = null, callable $cb = null)
+    {
+        $blist = new BList($blist);
+        
+        if ($cb) {
+            $blist = $cb($blist);
+        }
+        
+        return $blist;
+    }
+    
+    public static function dictionaryFactory($dict = null, callable $cb = null)
+    {
+        $dictionary = new Dictionary($dict);
+        
+        if ($cb) {
+            $dictionary = $cb($dictionary);
+        }
+        
+        return $dictionary;
+    }
 }

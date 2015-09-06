61a664327e5e5740f2b9972602f83fa425e7f17c
Dave Smith-Hayes <dsmithhayes@propellerideas.com>
2015-09-06 16:48:20 -0400

    Failed because I forgot how my own methods worked.

--- tests/BencodeIOTestCase.php
+++ tests/BencodeIOTestCase.php
@@ -10,9 +10,10 @@ class BencodeIOTestCase extends PHPUnit_Framework_TestCase
 {
     public function testBasicFileWriting()
     {
-        $blist = Bencode::blistFactory(function() {
-            return trim(file_get_contents(__DIR__ . '/assets/list.be'));
-        });
+        $file_stream = trim(file_get_contents(__DIR__ . '/assets/list.be'));
+        $blist = Bencode::blistFactory();
+        
+        $blist->decode($file_stream);
         
         $this->assertEquals('li1ei2e6:stringe', $blist->encode());
     }
